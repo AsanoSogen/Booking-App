9d0bea8fb8a4a34287eff078a4b4bcdb3cc313c0
AsanoSogen <AsanoSogen>
2020-02-20 16:59:04 +0900

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.12.0'
 
 # Capistranoのログの表示に利用する
-set :application, 'Booking-app'
+set :application, 'Booking-App'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:AsanoSogen/Booking-App.git'
